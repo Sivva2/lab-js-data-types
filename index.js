e0a7733a30b9ceb7fdbc902df883d38a52a9a05d
Kevin Abaskaran <shiva94460@gmail.com>
2024-08-28 12:25:17 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const result = `${s1} ${s2} ${s3} ${s4} ${s5}`
 
+console.log(result)
 
 // Print out the concatenated string
 
@@ -22,9 +24,13 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const result1 = part1.slice(0,-1) + part1.slice(-1).toUpperCase() 
+const result2 = part2.slice(0,-1) + part2.slice(-1).toUpperCase() 
 
 
 // Print the cameLtaiL-formatted string
+
+console.log(result1 + result2)
 
 
 
@@ -33,13 +39,14 @@ const part2 = "script";
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
+tipsAmount = billTotal * (15/100)
 
 // Calculate the tip (15% of the bill total)
 
 
 // Print out the tipAmount
 
-
+console.log(tipsAmount)
 
 
 /*******************************************
@@ -48,10 +55,10 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+randomNumber =  Math.round(Math.random() * 11)
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +81,20 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+1 = false
+2 = true
+3 = false
+4 = true
+5 = true
+6 = false
+7 = true
+
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
+console.log(expression5)
+console.log(expression6)
+console.log(expression7)
+
